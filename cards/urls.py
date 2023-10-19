e2826a5b08823e2f7a4e44269833c381808ac244
sphakamiso <sparngobese@gmail.com>
2023-10-20 00:58:21 +0200

    completed project

--- cards/urls.py
+++ cards/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
    path("", views.CardListView.as_view(),name='card-list'),
    path("new", views.CardCreateView.as_view(), name='card-create'),
    path("edit/<int:pk>", views.CardUpdateView.as_view(), name='card-update'),
+   path("box/<int:box_num>", views.BoxView.as_view(), name="box"),
 ]

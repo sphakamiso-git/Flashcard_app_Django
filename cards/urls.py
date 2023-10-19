ad750b612a011e9a4e07373ef38106194e44d0f7
sphakamiso <sparngobese@gmail.com>
2023-10-19 23:08:59 +0200

    second commit

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name='cards/base.html')),
+   # path("", TemplateView.as_view(template_name='cards/base.html')),
+   path("", views.CardListView.as_view(),name='card-list'),
+   path("new", views.CardCreateView.as_view(), name='card-create'),
+   path("edit/<int:pk>", views.CardUpdateView.as_view(), name='card-update'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import (ListView, CreateView, UpdateView)
+from .models import Card
+from django.urls import reverse_lazy
 
-# Create your views here.
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all().order_by('box', '-date_created')
+
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ['question', 'answer', 'box']
+    success_url = reverse_lazy('card-create')
+    
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy('card-list')
+
